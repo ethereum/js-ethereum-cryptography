cd424b870bb97f51bc41a55c23dd97dc4dcb8afa
Patricio Palladino <email@patriciopalladino.com>
2019-09-08 19:04:39 -0300

    Replace sha3 with keccak
    sha3 wasn't faster than the pure js implementation, keccak is.

--- packages/ethereum-cryptography-native/src/keccak.ts
+++ packages/ethereum-cryptography-native/src/keccak.ts
@@ -1,25 +1,25 @@
-const SHA3 = require("sha3");
+const createKeccakHash = require("keccak");
 
 export function keccak224(msg: Buffer): Buffer {
-  const sha3 = new SHA3.SHA3Hash(224);
-  sha3.update(msg);
-  return Buffer.from(sha3.digest(), "binary");
+  const hash = createKeccakHash("keccak224");
+  hash.update(msg);
+  return Buffer.from(hash.digest(), "binary");
 }
 
 export function keccak256(msg: Buffer): Buffer {
-  const sha3 = new SHA3.SHA3Hash(256);
-  sha3.update(msg);
-  return Buffer.from(sha3.digest(), "binary");
+  const hash = createKeccakHash("keccak256");
+  hash.update(msg);
+  return Buffer.from(hash.digest(), "binary");
 }
 
 export function keccak384(msg: Buffer): Buffer {
-  const sha3 = new SHA3.SHA3Hash(384);
-  sha3.update(msg);
-  return Buffer.from(sha3.digest(), "binary");
+  const hash = createKeccakHash("keccak384");
+  hash.update(msg);
+  return Buffer.from(hash.digest(), "binary");
 }
 
 export function keccak512(msg: Buffer): Buffer {
-  const sha3 = new SHA3.SHA3Hash(512);
-  sha3.update(msg);
-  return Buffer.from(sha3.digest(), "binary");
+  const hash = createKeccakHash("keccak512");
+  hash.update(msg);
+  return Buffer.from(hash.digest(), "binary");
 }
