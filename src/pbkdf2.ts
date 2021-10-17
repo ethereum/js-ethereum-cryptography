c0c68691e38d5983eabe595312f6b152b821732c
Paul Miller <paul@paulmillr.com>
2021-10-18 02:01:20 +0300

    Allow sha512 in pbkdf2

--- src/pbkdf2.ts
+++ src/pbkdf2.ts
@@ -3,6 +3,7 @@ import {
   pbkdf2Async as _pbkdf2Async
 } from "noble-hashes/lib/pbkdf2";
 import { sha256 } from "noble-hashes/lib/sha256";
+import { sha512 } from "noble-hashes/lib/sha512";
 import { assertBytes } from "./utils";
 
 export async function pbkdf2(
@@ -12,12 +13,12 @@ export async function pbkdf2(
   keylen: number,
   digest: string
 ): Promise<Uint8Array> {
-  if (digest !== "sha256") {
-    throw new Error("Only sha256 is supported");
+  if (!["sha256", "sha512"].includes(digest)) {
+    throw new Error("Only sha256 and sha512 are supported");
   }
   assertBytes(password);
   assertBytes(salt);
-  return _pbkdf2Async(sha256, password, salt, {
+  return _pbkdf2Async(digest === "sha256" ? sha256 : sha512, password, salt, {
     c: iterations,
     dkLen: keylen
   });
@@ -30,12 +31,12 @@ export function pbkdf2Sync(
   keylen: number,
   digest: string
 ): Uint8Array {
-  if (digest !== "sha256") {
-    throw new Error("Only sha256 is supported");
+  if (!["sha256", "sha512"].includes(digest)) {
+    throw new Error("Only sha256 and sha512 are supported");
   }
   assertBytes(password);
   assertBytes(salt);
-  return _pbkdf2(sha256, password, salt, {
+  return _pbkdf2(digest === "sha256" ? sha256 : sha512, password, salt, {
     c: iterations,
     dkLen: keylen
   });
