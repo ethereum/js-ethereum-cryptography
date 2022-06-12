65dad9490ba71da18d172436c79d0b3a420ebf15
Paul Miller <paul@paulmillr.com>
2022-06-12 13:47:20 +0400

    Expose _createKeccak256. Upgrade deps.

--- src/keccak.ts
+++ src/keccak.ts
@@ -1,7 +1,8 @@
-import * as sha3 from "@noble/hashes/sha3";
+import { keccak_224, keccak_256, keccak_384, keccak_512 } from "@noble/hashes/sha3";
 import { wrapHash } from "./utils";
 
-export const keccak224 = wrapHash(sha3.keccak_224);
-export const keccak256 = wrapHash(sha3.keccak_256);
-export const keccak384 = wrapHash(sha3.keccak_384);
-export const keccak512 = wrapHash(sha3.keccak_512);
+export const keccak224 = wrapHash(keccak_224);
+export const keccak256 = wrapHash(keccak_256);
+export const createKeccak256 = keccak_256.create;
+export const keccak384 = wrapHash(keccak_384);
+export const keccak512 = wrapHash(keccak_512);
