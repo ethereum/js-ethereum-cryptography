6275a0be240906acf50ce437490df1f1cdc2f822
Paul Miller <paul@paulmillr.com>
2022-07-14 10:18:51 +0000

    Try to fix webpack

--- test/karma.webpack.conf.js
+++ test/karma.webpack.conf.js
@@ -1,7 +1,7 @@
 module.exports = function(config) {
   config.set({
     frameworks: ["mocha"],
-    files: ["../test-builds/webpack-build.js"],
+    files: ["../test-builds/main.js"],
     colors: true,
     logLevel: config.LOG_INFO,
     browsers: ["ChromeHeadless"],
