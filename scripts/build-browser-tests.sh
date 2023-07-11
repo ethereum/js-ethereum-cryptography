f198641616b2fe49f4857825a06a5c0af3f3b44e
Paul Miller <paul@paulmillr.com>
2023-07-11 22:24:02 +0000

    Compile tests on their own

--- scripts/build-browser-tests.sh
+++ scripts/build-browser-tests.sh
@@ -1,16 +1,22 @@
 set -e
 
+cd ./test/
+echo "Install package to tests"
+# Cleanup old module build
+rm -rf ./node_modules
+npm install
+
 echo "Building tests with TypeScript"
-npx tsc --project tsconfig.json
+npx tsc --project ./tsconfig.json
 
 echo "Building tests with Parcel"
-npx parcel build --no-cache --no-optimize test-builds/tsc/test/test-vectors/*.js --dist-dir test-builds/parcel --target parcel_tests
+npx parcel build --no-cache --no-optimize ./test-builds/tsc/test/test-vectors/*.js --dist-dir ./test-builds/parcel --target parcel_tests
 
 echo "Building tests with Browserify"
-npx browserify test-builds/tsc/test/test-vectors/*.js > test-builds/browserify-build.js
+npx browserify ./test-builds/tsc/test/test-vectors/*.js > ./test-builds/browserify-build.js
 
 echo "Building tests with webpack"
 npx webpack --mode development ./test-builds/tsc/test/test-vectors/*.js --output-path ./test-builds
 
 echo "Building tests with Rollup"
-rollup -c test/rollup.config.js
+rollup -c ./rollup.config.js
