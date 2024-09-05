6a7911d14b3ee8506f75a2890f5329516be44be1
legobt <6wbvkn0j@anonaddy.me>
2024-09-05 09:56:25 +0000

    export modules from index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-throw new Error(
-  "This package has no entry-point. Please consult the README.md to learn how to use it."
-);
+export * as aes from "./aes";
+export * as bip39 from "./bip39";
+export * as blake2b from "./blake2b";
+export * as bls from "./bls";
+export * as bn from "./bn";
+export * as hdkey from "./hdkey";
+export * as keccak from "./keccak";
+export * as math from "./math";
+export * as pbkdf2 from "./pbkdf2";
+export * as random from "./random";
+export * as ripemd160 from "./ripemd160";
+export * as scrypt from "./scrypt";
+export * as secp256k1 from "./secp256k1";
+export * as sha256 from "./sha256";
+export * as sha512 from "./sha512";
+export * as utils from "./utils";
