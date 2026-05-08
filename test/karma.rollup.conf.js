80f4259bddac83a40019fb7ca82064f319ea2f3d
Paul Miller <paul@paulmillr.com>
2026-05-08 00:01:13 +0000

    Update test configs to new packages

--- test/karma.rollup.conf.js
+++ test/karma.rollup.conf.js
@@ -4,6 +4,9 @@ module.exports = function (config) {
     files: ["./test-builds/rollup/*.js"],
     colors: true,
     logLevel: config.LOG_INFO,
+    // Do not expose the Karma test server on all interfaces.
+    hostname: "127.0.0.1",
+    listenAddress: "127.0.0.1",
     browsers: ["ChromeHeadless"],
     browserDisconnectTimeout: 100000,
     browserDisconnectTolerance: 3,
